cf9785f9add502bc4b91873ea2b3a64fb556a0ef
Edwin Jones <edwinjones@outlook.com>
2018-01-21 20:25:31 +0000

    first pass at swagger implementation

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+
+var swaggerDefinition = {
+    info: {
+        title: 'Polite Catfacts API',
+        version: '1.0.0',
+        description: 'A simple api for getting nice and polite catfacts',
+    },
+    host: `'/:${port}`,
+    basePath: '/',
+};
 
 //import routes
 const routes = require('./api/routes/catfactRoutes');
 routes(app);
 
+//setup swagger
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+//start
 app.listen(port);
 
-console.log('cat fact RESTful API server started on: ' + port);
+console.log('polite-catfact RESTful API server started on: ' + port);
